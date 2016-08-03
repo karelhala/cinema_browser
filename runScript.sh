dc797dfe37e9d3a6a85f7b55d8601c5e561d5fda
Karel Hala <khala@redhat.com>
2016-08-03 15:47:24 +0200

    Change fetch html to json

--- runScript.sh
+++ runScript.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 for CINEMA_ID in 1010103 1010107
 do
-    for DAY in 0 1 2 3 4 5
-    do
-        FETCH_DATE=`date -d "+$DAY days" +%d/%m/%Y`
-        TIMESTAMP=`date -d ${FETCH_DATE} +"%s"`
-        wget "http://www.cinemacity.cz/scheduleInfo?locationId=$CINEMA_ID&date=$FETCH_DATE&hideSite=1" -O "htmlData/$CINEMA_ID/$TIMESTAMP.html"
-    done
+    wget "http://www.cinemacity.cz/upcommingJSON?includeVenueName=true&subSiteId=$CINEMA_ID&days=5&showExpired=true" -O "data/$CINEMA_ID.json"
 done
 
