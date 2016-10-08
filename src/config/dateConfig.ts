58355666fa6c620b4b9523ebfac36b1e56f2992b
Karel Hala <khala@redhat.com>
2016-10-08 23:42:01 +0200

    Update styling of records

--- src/config/dateConfig.ts
+++ src/config/dateConfig.ts
@@ -6,9 +6,10 @@ export default (module: ng.IModule) => {
     $mdDateLocaleProvider.formatDate = (date) => {
       return date ? moment(date).format('DD.MM.YYYY') : '';
     };
+
     $mdDateLocaleProvider.parseDate = (dateString) => {
-      var m = moment(dateString, 'DD.MM.YYYY', true);
+      let m = moment(dateString, 'DD.MM.YYYY', true);
       return m.isValid() ? m.toDate() : new Date(NaN);
     };
-  })
+  });
 }
