f7d39aecfff29a4d175f3e4a9b5ecb44f1191dca
Karel Hala <khala@redhat.com>
2019-10-29 15:22:51 +0100

    Update release.js

--- release.js
+++ release.js
@@ -10,21 +10,48 @@ const writeFile = (fileName, data) => util.promisify(fs.writeFile)(fileName, dat
 const readJson = (fileName) => readFile(fileName).then(JSON.parse);
 const writeJson = (fileName, data) => writeFile(fileName, JSON.stringify(data, null, 4) + '\n');
 
+dotenv.config();
 const Octokit = require("@octokit/rest");
 const octokit = Octokit({
     auth: process.env.GH_TOKEN,
     userAgent: 'karelhala',
     previews: ['jean-grey', 'symmetra'],
     timeZone: 'Europe/Prague',
-    baseUrl: 'https://api.github.com',
+    baseUrl: 'https://api.github.com'
 });
 
 const randomValue = Math.random().toString(36).substring(Math.round(Math.random() * Math.floor(10)));
+
+const encodeFile = async (filename) => Buffer.from(await readFile(filename)).toString('base64');
 
 (async () => {
     const dump = await readJson('./dump.json');
 
     dump.value = randomValue;
 
-    writeJson('./dump.json', dump);
+    await writeJson('./dump.json', dump);
+    let sha;
+    try {
+        const { data: contents } = await octokit.repos.getContents({
+            owner: 'karelhala',
+            repo: 'cinema_browser',
+            path: 'dump.json'
+        });
+        sha = contents && contents.sha;
+    } catch(e) {}
+
+
+    const content = await encodeFile('./dump.json');
+
+    console.log(sha);
+    console.log(content);
+    octokit.repos.createOrUpdateFile({
+        owner: 'karelhala',
+        repo: 'cinema_browser',
+        path: 'dump.json',
+        message: 'Update dump file from bot!',
+        content: content,
+        debug: true,
+        ...sha && { sha }
+    }).then(() => {}, console.log)
 })();
